adb31c8e62240fe9e6bb066b4122380d789bda50
Aleksander Khanteev <aleksander_khanteev@epam.com>
2019-07-15 18:30:50 +0400

    Naive saga implementation.
    Supports:
    * Sync\async activity execution with compensation capture.
    * Async child workflow execution with compensation capture.
    * Compensates current and child workflow activities recursively.

---
+++ src/main/java/com/uber/cadence/samples/hellosaga/HelloSaga.java
@@ -0,0 +1,165 @@
+/*
+ *  Copyright 2012-2016 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ *  Modifications copyright (C) 2017 Uber Technologies, Inc.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"). You may not
+ *  use this file except in compliance with the License. A copy of the License is
+ *  located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ *  or in the "license" file accompanying this file. This file is distributed on
+ *  an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ *  express or implied. See the License for the specific language governing
+ *  permissions and limitations under the License.
+ */
+
+package com.uber.cadence.samples.hellosaga;
+
+import static com.uber.cadence.samples.common.SampleConstants.DOMAIN;
+
+import com.uber.cadence.activity.ActivityMethod;
+import com.uber.cadence.client.WorkflowClient;
+import com.uber.cadence.samples.hellosaga.saga.CompensationWorkflowImpl;
+import com.uber.cadence.samples.hellosaga.saga.Saga;
+import com.uber.cadence.worker.Worker;
+import com.uber.cadence.workflow.*;
+
+public class HelloSaga {
+  static final String TASK_LIST = "HelloSaga";
+
+  /** The parent workflow interface. */
+  public interface GreetingWorkflow {
+    /** @return greeting string */
+    @WorkflowMethod(executionStartToCloseTimeoutSeconds = 10, taskList = TASK_LIST)
+    String getGreeting(String name);
+  }
+
+  public interface GreetingActivities {
+    @ActivityMethod(scheduleToCloseTimeoutSeconds = 10)
+    Saga.Result<String, String> makeGreeting(String name);
+
+    @ActivityMethod(scheduleToCloseTimeoutSeconds = 10)
+    void makeGreetingCompensation(String name);
+  }
+
+  /** The child workflow interface. */
+  public interface GreetingChild {
+    @WorkflowMethod
+    Saga.WorkflowResult<String> composeGreeting(String name);
+  }
+
+  public interface GreetingChildActivities {
+    @ActivityMethod(scheduleToCloseTimeoutSeconds = 10)
+    Saga.Result<String, String> makeGreeting(String name);
+
+    @ActivityMethod(scheduleToCloseTimeoutSeconds = 10)
+    void makeGreetingCompensation(String name);
+  }
+
+  /** GreetingWorkflow implementation that calls GreetingsActivities#printIt. */
+  public static class GreetingWorkflowImpl implements GreetingWorkflow {
+
+    private final Saga saga = new Saga();
+    private final GreetingActivities activities =
+        Workflow.newActivityStub(GreetingActivities.class);
+    private final GreetingChild child = Workflow.newChildWorkflowStub(GreetingChild.class);
+
+    @Override
+    public String getGreeting(String name) {
+
+      try {
+        // Executing an action with compensation
+        String parentGreeting =
+            saga.executeFunc(activities::makeGreeting, name, activities::makeGreetingCompensation);
+
+        // Executing a child workflow with compensation
+        String childGreeting = saga.executeChildFuncAsync(child::composeGreeting, name).get();
+
+        return parentGreeting + " and " + childGreeting;
+      } catch (ActivityFailureException ex) {
+        // In case of error in parent workflow - compensate everything
+        saga.compensate();
+        return "Epic fail :(";
+      }
+    }
+  }
+
+  public static class GreetingActivitiesImpl implements GreetingActivities {
+
+    @Override
+    public Saga.Result<String, String> makeGreeting(String name) {
+      System.out.println("GreetingActivitiesImpl::makeGreeting");
+      return new Saga.Result<>("Hello " + name, name);
+    }
+
+    @Override
+    public void makeGreetingCompensation(String name) {
+      System.out.println("GreetingActivitiesImpl::makeGreetingCompensation");
+    }
+  }
+
+  /**
+   * The child workflow implementation. A workflow implementation must always be public for the
+   * Cadence library to be able to create instances.
+   */
+  public static class GreetingChildImpl implements GreetingChild {
+
+    private final Saga saga = new Saga();
+    private final GreetingChildActivities activities =
+        Workflow.newActivityStub(GreetingChildActivities.class);
+
+    @Override
+    public Saga.WorkflowResult<String> composeGreeting(String name) {
+      try {
+        // Execute an action with compensation
+        String greeting =
+            saga.executeFunc(activities::makeGreeting, name, activities::makeGreetingCompensation);
+        // Return all the data required for compensation of the whole workflow
+        return new Saga.WorkflowResult<>(greeting, saga.exportCompensations());
+      } catch (ActivityFailureException ex) {
+        // In case of error in a child workflow - compensate own actions and rethrow
+        saga.compensate();
+        throw ex;
+      }
+    }
+  }
+
+  public static class GreetingChildActivitiesImpl implements GreetingChildActivities {
+    @Override
+    public Saga.Result<String, String> makeGreeting(String name) {
+      System.out.println("GreetingChildActivitiesImpl::makeGreeting");
+      return new Saga.Result<>("Hello from child " + name, name);
+    }
+
+    @Override
+    public void makeGreetingCompensation(String name) {
+      System.out.println("GreetingChildActivitiesImpl::makeGreetingCompensation");
+    }
+  }
+
+  public static void main(String[] args) {
+    // Start a worker that hosts both parent and child workflow implementations.
+    Worker.Factory factory = new Worker.Factory(DOMAIN);
+    Worker worker = factory.newWorker(TASK_LIST);
+    worker.registerWorkflowImplementationTypes(
+        GreetingWorkflowImpl.class,
+        GreetingChildImpl.class,
+        // register a standard compensation workflow
+        CompensationWorkflowImpl.class);
+    worker.registerActivitiesImplementations(
+        new GreetingActivitiesImpl(), new GreetingChildActivitiesImpl());
+    // Start listening to the workflow task list.
+    factory.start();
+
+    // Start a workflow execution. Usually this is done from another program.
+    WorkflowClient workflowClient = WorkflowClient.newInstance(DOMAIN);
+    // Get a workflow stub using the same task list the worker uses.
+    GreetingWorkflow workflow = workflowClient.newWorkflowStub(GreetingWorkflow.class);
+    // Execute a workflow waiting for it to complete.
+    String greeting = workflow.getGreeting("World");
+    System.out.println(greeting);
+    System.exit(0);
+  }
+}
---
+++ src/main/java/com/uber/cadence/samples/hellosaga/saga/Saga.java
@@ -0,0 +1,273 @@
+/*
+ *  Copyright 2012-2016 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ *  Modifications copyright (C) 2017 Uber Technologies, Inc.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"). You may not
+ *  use this file except in compliance with the License. A copy of the License is
+ *  located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ *  or in the "license" file accompanying this file. This file is distributed on
+ *  an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ *  express or implied. See the License for the specific language governing
+ *  permissions and limitations under the License.
+ */
+
+package com.uber.cadence.samples.hellosaga.saga;
+
+import com.uber.cadence.activity.ActivityOptions;
+import com.uber.cadence.internal.common.LambdaUtils;
+import com.uber.cadence.workflow.*;
+import java.lang.invoke.SerializedLambda;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Proxy;
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Stack;
+
+public class Saga {
+
+  private Stack<CompensationItem> compensations = new Stack<>();
+
+  public <A1, CA1> void executeProc(
+      Functions.Func1<A1, Compensation<CA1>> proc, A1 arg1, Functions.Proc1<CA1> compensationProc) {
+    Compensation<CA1> result = proc.apply(arg1);
+    compensations.push(
+        new CompensationItem(
+            getExecutionName(compensationProc),
+            ExecutionType.ACTIVITY,
+            result.compensationArgs,
+            null));
+  }
+
+  public <A1, CA1> Promise<Void> executeProcAsync(
+      Functions.Func1<A1, Compensation<CA1>> proc, A1 arg1, Functions.Proc1<CA1> compensationProc) {
+    return Async.function(proc, arg1)
+        .thenApply(
+            result -> {
+              compensations.push(
+                  new CompensationItem(
+                      getExecutionName(compensationProc),
+                      ExecutionType.ACTIVITY,
+                      result.compensationArgs,
+                      null));
+              return null;
+            });
+  }
+
+  public <A1, CA1, R> Promise<R> executeFuncAsync(
+      Functions.Func1<A1, Result<R, CA1>> func, A1 arg1, Functions.Proc1<CA1> compensationProc) {
+    return Async.function(func, arg1)
+        .thenApply(
+            result -> {
+              compensations.push(
+                  new CompensationItem(
+                      getExecutionName(compensationProc),
+                      ExecutionType.ACTIVITY,
+                      result.compensationArgs,
+                      null));
+              return result.getResult();
+            });
+  }
+
+  public <A1, CA1, R> R executeFunc(
+      Functions.Func1<A1, Result<R, CA1>> func, A1 arg1, Functions.Proc1<CA1> compensationProc) {
+    Result<R, CA1> result = func.apply(arg1);
+    compensations.push(
+        new CompensationItem(
+            getExecutionName(compensationProc),
+            ExecutionType.ACTIVITY,
+            result.compensationArgs,
+            null));
+    return result.getResult();
+  }
+
+  public <A1, R> Promise<R> executeChildFuncAsync(
+      Functions.Func1<A1, WorkflowResult<R>> func, A1 arg1) {
+    return Async.function(func, arg1)
+        .thenApply(
+            result -> {
+              compensations.push(
+                  new CompensationItem(
+                      null,
+                      ExecutionType.CHILD_WORKFLOW,
+                      result.compensationArgs,
+                      getChildWfTaskList(func)));
+              return result.getResult();
+            });
+  }
+
+  public List<CompensationItem> exportCompensations() {
+    return new ArrayList<>(compensations);
+  }
+
+  public void importCompensations(List<CompensationItem> compensations) {
+    this.compensations.addAll(compensations);
+  }
+
+  public void compensate() {
+
+    ActivityStub activityExecutor =
+        Workflow.newUntypedActivityStub(
+            new ActivityOptions.Builder()
+                .setScheduleToCloseTimeout(Duration.ofSeconds(10))
+                .setScheduleToStartTimeout(Duration.ofSeconds(10))
+                .build());
+
+    while (!compensations.empty()) {
+      CompensationItem comp = compensations.pop();
+      switch (comp.getExecutionType()) {
+        case ACTIVITY:
+          activityExecutor.execute(comp.getCompProc(), Void.class, comp.getCompArg());
+          break;
+        case CHILD_WORKFLOW:
+          CompensationWorkflow compensationWfExecutor =
+              Workflow.newChildWorkflowStub(
+                  CompensationWorkflow.class,
+                  new ChildWorkflowOptions.Builder().setTaskList(comp.getTaskList()).build());
+          compensationWfExecutor.compensate((List<CompensationItem>) comp.getCompArg());
+          break;
+        default:
+          System.out.printf("Unhandled execution type: %s\n", comp.getExecutionType());
+          break;
+      }
+    }
+  }
+
+  public enum ExecutionType {
+    ACTIVITY,
+    CHILD_WORKFLOW
+  }
+
+  private String getExecutionName(Object compensationProc) {
+    SerializedLambda lambda = LambdaUtils.toSerializedLambda(compensationProc);
+    String className =
+        Arrays.stream(lambda.getImplClass().split("/")).reduce((first, second) -> second).get();
+    String methodName = lambda.getImplMethodName();
+    // does not takes into account activity name at annotation, taken from InternalUtils
+    return className + "::" + methodName;
+  }
+
+  private String getChildWfTaskList(Object compensationProc) {
+    try {
+      // A hack to extract task queue for a compensation
+      SerializedLambda lambda = LambdaUtils.toSerializedLambda(compensationProc);
+      Object proxy = lambda.getCapturedArg(0);
+      InvocationHandler handler = Proxy.getInvocationHandler(proxy);
+      Field stubField = handler.getClass().getDeclaredField("stub");
+      stubField.setAccessible(true);
+      ChildWorkflowStub target = (ChildWorkflowStub) stubField.get(handler);
+      return target.getOptions().getTaskList();
+    } catch (Exception ex) {
+      throw new RuntimeException(ex);
+    }
+  }
+
+  public static class CompensationItem {
+    private String compProc;
+    private ExecutionType executionType;
+    private Object compArg;
+    private String taskList;
+
+    public CompensationItem(
+        String compProc, ExecutionType executionType, Object compArg, String taskList) {
+      this.compProc = compProc;
+      this.executionType = executionType;
+      this.compArg = compArg;
+      this.taskList = taskList;
+    }
+
+    public String getCompProc() {
+      return compProc;
+    }
+
+    public void setCompProc(String compProc) {
+      this.compProc = compProc;
+    }
+
+    public ExecutionType getExecutionType() {
+      return executionType;
+    }
+
+    public void setExecutionType(ExecutionType executionType) {
+      this.executionType = executionType;
+    }
+
+    public Object getCompArg() {
+      return compArg;
+    }
+
+    public void setCompArg(Object compArg) {
+      this.compArg = compArg;
+    }
+
+    public String getTaskList() {
+      return taskList;
+    }
+
+    public void setTaskList(String taskList) {
+      this.taskList = taskList;
+    }
+  }
+
+  public static class Result<TResult, TCompensationArgs> {
+    private TResult result;
+    private TCompensationArgs compensationArgs;
+
+    public Result(TResult result, TCompensationArgs compensationArgs) {
+      this.result = result;
+      this.compensationArgs = compensationArgs;
+    }
+
+    public TResult getResult() {
+      return result;
+    }
+
+    public void setResult(TResult result) {
+      this.result = result;
+    }
+
+    public TCompensationArgs getCompensationArgs() {
+      return compensationArgs;
+    }
+
+    public void setCompensationArgs(TCompensationArgs compensationArgs) {
+      this.compensationArgs = compensationArgs;
+    }
+  }
+
+  public static class WorkflowResult<TResult> {
+    private TResult result;
+    private List<CompensationItem> compensationArgs;
+
+    public WorkflowResult(TResult result, List<CompensationItem> compensationArgs) {
+      this.result = result;
+      this.compensationArgs = compensationArgs;
+    }
+
+    public TResult getResult() {
+      return result;
+    }
+
+    public void setResult(TResult result) {
+      this.result = result;
+    }
+
+    public List<CompensationItem> getCompensationArgs() {
+      return compensationArgs;
+    }
+
+    public void setCompensationArgs(List<CompensationItem> compensationArgs) {
+      this.compensationArgs = compensationArgs;
+    }
+  }
+
+  public static class Compensation<TCompensationArgs> {
+    private TCompensationArgs compensationArgs;
+  }
+}
