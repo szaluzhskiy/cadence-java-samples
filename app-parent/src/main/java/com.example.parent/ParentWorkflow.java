4757186bb9d88eb74dba40c535e4b079f0bfa5d7
Elena_Nazarova <Elena_Nazarova@epam.com>
2020-05-21 14:24:06 +0300

    Added parent activities

--- app-parent/src/main/java/com.example.parent/ParentWorkflow.java
+++ app-parent/src/main/java/com.example.parent/ParentWorkflow.java
@@ -19,7 +19,11 @@ package com.example.parent;
 
 import com.uber.cadence.DomainAlreadyExistsError;
 import com.uber.cadence.RegisterDomainRequest;
+import com.uber.cadence.activity.Activity;
+import com.uber.cadence.activity.ActivityMethod;
+import com.uber.cadence.activity.ActivityOptions;
 import com.uber.cadence.client.WorkflowClient;
+import com.uber.cadence.common.RetryOptions;
 import com.uber.cadence.serviceclient.IWorkflowService;
 import com.uber.cadence.serviceclient.WorkflowServiceTChannel;
 import com.uber.cadence.worker.Worker;
@@ -34,7 +38,8 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
-import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.List;
 
 import static com.example.parent.SampleConstants.*;
 
@@ -64,10 +69,14 @@ public class ParentWorkflow implements ApplicationRunner {
             7933,
             DOMAIN,
             new Worker.FactoryOptions.Builder().setMetricScope(scope).build());
+
     Worker workerParent =
         factory.newWorker(
-            TASK_LIST_PARENT, new WorkerOptions.Builder().setMetricsScope(scope).build());
+            TASK_LIST_PARENT, new WorkerOptions.Builder().setMetricsScope(scope).build()
+        );
+
     workerParent.registerWorkflowImplementationTypes(GreetingWorkflowImpl.class);
+    workerParent.registerActivitiesImplementations(new ParentActivitiesImpl());
 
     // Start listening to the workflow and activity task lists.
     factory.start();
@@ -131,6 +140,21 @@ public class ParentWorkflow implements ApplicationRunner {
     String composeGreeting(String greeting, String name);
   }
 
+  public interface ParentActivities {
+    @ActivityMethod(scheduleToStartTimeoutSeconds = 60000, startToCloseTimeoutSeconds = 60)
+    String composeParentGreeting(int activityIdx);
+  }
+
+  static class ParentActivitiesImpl implements ParentActivities {
+    @Override
+    public String composeParentGreeting(int activityIdx) {
+//      System.out.printf("[%d] Parent activity done\n", activityIdx);
+
+      return String.format("Finished parent activity: idx: [%d], activity id: [%s], task: [%s]", activityIdx,
+              Activity.getTask().getActivityId(), new String(Activity.getTaskToken()));
+    }
+  }
+
   /** GreetingWorkflow implementation that calls GreetingsActivities#printIt. */
   public static class GreetingWorkflowImpl implements GreetingWorkflow {
 
@@ -145,10 +169,34 @@ public class ParentWorkflow implements ApplicationRunner {
 
       // This is a blocking call that returns only after the child has completed.
       Promise<String> greeting = Async.function(child::composeGreeting, "Hello", name);
+
       // Do something else here.
+      Promise<List<String>> parentPromises = runParentActivities();
+      System.out.println("Got result in parent: " + String.join(";\n", parentPromises.get()) + "\n");
 
       return greeting.get(); // blocks waiting for the child to complete.
-      // return "Child started";
+    }
+
+    private Promise<List<String>> runParentActivities() {
+      RetryOptions ro =
+              new RetryOptions.Builder()
+                      .setInitialInterval(java.time.Duration.ofSeconds(30))
+                      .setMaximumInterval(java.time.Duration.ofSeconds(30))
+                      .setMaximumAttempts(2)
+                      .build();
+      ActivityOptions ao =
+              new ActivityOptions.Builder()
+                      .setTaskList(TASK_LIST_PARENT)
+                      .setRetryOptions(ro)
+                      .build();
+
+      List<Promise<String>> parentActivities = new ArrayList<>();
+      for (int i = 0; i < 4; i++) {
+        ParentActivities activity = Workflow.newActivityStub(ParentActivities.class, ao);
+        parentActivities.add(Async.function(activity::composeParentGreeting, i));
+      }
+
+      return Promise.allOf(parentActivities);
     }
   }
 }
